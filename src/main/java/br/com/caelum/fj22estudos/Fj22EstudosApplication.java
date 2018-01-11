ab5cc2f1042b50e3a1b761fdc76f73525b6714da
Matheus Brandino <m.v.brandino@gmail.com>
2018-01-11 19:48:12 -0200

    first commit

---
+++ src/main/java/br/com/caelum/fj22estudos/Fj22EstudosApplication.java
@@ -0,0 +1,12 @@
+package br.com.caelum.fj22estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fj22EstudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fj22EstudosApplication.class, args);
+	}
+}
