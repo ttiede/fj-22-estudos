3986bd3e04740b82f248c43e768189c53a56f2cb
Tiago Tiede <geschopf@gmail.com>
2018-01-18 22:22:53 -0200

    Fix name of Interface

--- src/main/java/repository/IRoleRepository.java
+++ src/main/java/repository/IRoleRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository("roleRepository")
-public interface RoleRepository extends JpaRepository<Role, Integer>{
+public interface IRoleRepository extends JpaRepository<Role, Integer>{
     Role findByRole(String role);
 
 }
